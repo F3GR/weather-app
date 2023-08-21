aa52c70f880e94ce93893a3f5df05dd194cc2152
F3GR <egmver.2@gmail.com>
2023-08-21 21:36:10 +0400

    template

--- test.js
+++ test.js
@@ -1,20 +1,2 @@
 import './reset.css'
 import './style.css'
-
-const img = document.querySelector('img');
-const button = document.querySelector('button.new-image')
-button.addEventListener('click', (e) => {
-    const text = document.querySelector('#gif-query').value
-    fetchNewImageSrc(img, text)
-})
-
-function fetchNewImageSrc(element,text) {
-    element.src = ''
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=vKtQCOzfde0ZpQH426p1RN8qx1dqYuAD&s=${text}`)
-    .then(function(response) {
-    return response.json()
-    })
-    .then(function(response) {
-    element.src = response.data.images.original.url
-    })
-}
